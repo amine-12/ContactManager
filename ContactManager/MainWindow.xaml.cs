ac129b96fb920cbb3b6a1f5ee98b50c80ec381ed
amine-12 <54552812+amine-12@users.noreply.github.com>
2020-11-30 17:54:28 -0500

    Added double click event to diplay

--- ContactManager/MainWindow.xaml.cs
+++ ContactManager/MainWindow.xaml.cs
@@ -24,13 +24,23 @@ namespace ContactManager
         public MainWindow()
         {
             InitializeComponent();
+            
             contact.Add(new Contact() { FirstName = "Bob", LastName = "Leponge ", PhoneNumber = "123 456 789", Email = "asads@gmail.com", Address = "123 Street" });
             contact.Add(new Contact() { FirstName = "Qwer", LastName = "Ty", PhoneNumber = "545 456 789", Email = "adaeds@gmail.com", Address = "12334 Street" });
             contact.Add(new Contact() { FirstName = "Tpop", LastName = "Hjkg", PhoneNumber = "123 516 789", Email = "gdsa@gmail.com", Address = "14223 Street" });
             contact.Add(new Contact() { FirstName = "Lojk", LastName = "Poip", PhoneNumber = "123 456 945", Email = "ertf@gmail.com", Address = "1473 Street" });
             contactList.ItemsSource = contact;
+            contactList.MouseDoubleClick +=  HandleDoubleClick;
         }
-        
+
+        private void HandleDoubleClick(object sender, RoutedEventArgs e)
+        {
+            if (contactList.SelectedIndex >= 0)
+            {
+                DisplayContact displayContact = new DisplayContact(contactList.SelectedIndex);
+                displayContact.Show();
+            }
+        }
 
         private void editContact_Click(object sender, RoutedEventArgs e)
         {
@@ -62,14 +72,6 @@ namespace ContactManager
             //    Console.WriteLine(x.ToString());
             //    Console.WriteLine("----------------------");
             //}
-        }
-
-        private void displayContact_Click(object sender, RoutedEventArgs e)
-        {
-            if(contactList.SelectedIndex >= 0) { 
-                DisplayContact displayContact = new DisplayContact(contactList.SelectedIndex);
-                displayContact.Show();
-            }
         }
 
         private void addContact_Click(object sender, RoutedEventArgs e)
